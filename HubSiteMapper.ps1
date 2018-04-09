c19ea86bdb24248dab89c1b7fd65340bf6377877
Martin White <martin.white@intelogy.co.uk>
2018-04-09 09:54:07 +0100

    Description updated

--- HubSiteMapper.ps1
+++ HubSiteMapper.ps1
@@ -17,7 +17,15 @@
  	Author: Martin White
  	Creation Date: 06/04/2018
  	Purpose/Change: Initial script development
- 	 
+ 	The sample scripts are not supported under any Intelogy standard support program or service. 
+    The sample scripts are provided AS IS without warranty of any kind. Intelogy further disclaims all implied warranties including, 
+    without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out 
+    of the use or performance of the sample scripts and documentation remains with you. In no event shall Intelogy, its authors, 
+    or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, 
+    without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) 
+    arising out of the use of or inability to use the sample scripts or documentation, even if Intelogy has been advised of the possibility 
+    of such damages. 
+
  	.EXAMPLE
  	None
 #>
